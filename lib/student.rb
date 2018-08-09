23ce0e7ea833feba19412f03ccb2a45c83d09851
Laura Messa Travin <laura.travin@gmail.com>
2018-08-09 06:12:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,14 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+               attributes_hash.each{|chr|
+                  @twitter=  chr[:twitter] if chr[:twitter]
+                  @linkedin= chr[:linkedin] if chr[:linkedin]
+                  @github= chr[:github] if chr[:github]
+                  @blog= chr[:blog] if chr[:blog]
+                  @profile_quote= chr[:profile_quote] 
+                  @bio= chr[:bio]
+               }
   end
 
   def self.all
