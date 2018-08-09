1cda910d1f7edfcd38d8ae70f8ac4b414098f45b
Laura Messa Travin <laura.travin@gmail.com>
2018-08-09 05:57:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,8 @@ class Student
 
   def initialize(student_hash)
     @name= student_hash[:name]
+    @location= student_hash[:location]
+    @profile_url= student_hash[:profile_url]
   end
 
   def self.create_from_collection(students_array)
