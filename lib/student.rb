bdfc87669be8d2a8b389c8b98ca27197a0b7ff6d
Laura Messa Travin <laura.travin@gmail.com>
2018-08-09 06:34:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
                attributes_hash.each{|k,v|
-                 binding.pry
+                
                                self.send(k.to_s+"=",v)
                   # @twitter=  v if k.to_s == "twitter"
                   #
